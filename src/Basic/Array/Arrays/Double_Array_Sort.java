980e0e04b94690ab4dc4b05dc292ae22959ae43a
siweishengwu <ilu.zeff@gmail.com>
2019-11-26 23:00:24 +0800

    11.26

--- src/Basic/Array/Arrays/Double_Array_Sort.java
+++ src/Basic/Array/Arrays/Double_Array_Sort.java
@@ -6,6 +6,7 @@ public class Double_Array_Sort {
     public static void main(String[] args) {
         int a[][] = new int[5][8];
         int b[] = new int[a.length*a[0].length];
+
         for (int i = 0; i < 5 ; i++) {
             for (int j = 0; j < 8 ; j++) {
                 a[i][j] = (int)(Math.random()*100);
@@ -22,6 +23,7 @@ public class Double_Array_Sort {
         for (int i = 0; i <a.length ; i++) {
             System.arraycopy(a[i],0,b,i*a[i].length,a[i].length);
         }
+
         System.out.println(" ");
         System.out.println(Arrays.toString(b));
         Arrays.sort(b);
