4bfd17fd86042e08b816935770c65db3d839787c
zeff <ilu.zeff@gmail.com>
2019-12-06 17:46:59 +0800

    12.5

--- src/Basic/Class_And_Object/Attribute_Initialization/Hero.java
+++ src/Basic/Class_And_Object/Attribute_Initialization/Hero.java
@@ -1,11 +1,11 @@
 package Basic.Class_And_Object.Attribute_Initialization;
 
 public class Hero {
-    {
-        name = getName("初始化块");
-    }
     public Hero(){
         name = getName("构造方法");
+    }
+    {
+        name = getName("初始化块");
     }
     public String name = getName("属性声明");
     {
