227a80acfea4aaa59fe127c3099585775b2fffc9
siweishengwu <ilu.zeff@gmail.com>
2020-01-08 21:36:39 +0800

    2020，1/8，fighting

---
+++ src/Basic/NumbersAndStrings/Digit/TestNumer.java
@@ -0,0 +1,9 @@
+package Basic.NumbersAndStrings.Digit;
+
+public class TestNumer {
+    public static void main(String[] args) {
+        int i = 5;
+        Integer it = new Integer(i);
+        int i2 = it.intValue();
+    }
+}
