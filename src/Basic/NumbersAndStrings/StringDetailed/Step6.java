b05d1a7ac57b15771b82403c81bf825b38e3bbe6
zeff <ilu.zeff@gmail.com>
2020-01-17 17:59:54 +0800

    2020-1-17

---
+++ src/Basic/NumbersAndStrings/StringDetailed/Step6.java
@@ -0,0 +1,11 @@
+package Basic.NumbersAndStrings.StringDetailed;
+
+public class Step6 {
+    public static void main(String[] args) {
+        char c ='A';
+        String a ="Merry Christmas";
+        System.out.println(c);
+        short s = (short)c;
+        System.out.println(s);
+    }
+}
