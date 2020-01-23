b0ab8471968406b1fd4bdc7d8964fd2446e71a28
siweishengwu <ilu.zeff@gmail.com>
2020-01-23 16:31:15 +0800

    2020，1/23 time so many fast, today you code??

---
+++ src/Basic/NumbersAndStrings/Math/Step2.java
@@ -0,0 +1,11 @@
+package Basic.NumbersAndStrings.Math;
+
+public class Step2 {
+    public static void main(String[] args) {
+        System.out.println(Math.E);
+
+        int  n = 10;
+        double e = Math.pow(1+1/n,n);
+        System.out.println(e);
+    }
+}
