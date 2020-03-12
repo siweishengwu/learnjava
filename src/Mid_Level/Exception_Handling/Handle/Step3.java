08a9b9d442d8f16a4b90ddb813f290cd18b906fe
zeff <ilu.zeff@gmail.com>
2020-03-12 17:23:09 +0800

    2020-3-12 code is life

--- src/Mid_Level/Exception_Handling/Handle/Step3.java
+++ src/Mid_Level/Exception_Handling/Handle/Step3.java
@@ -9,14 +9,14 @@ import java.util.Date;
 
 public class Step3 {
     public static void main(String[] args) {
-        File f = new File("d:/1.exe");
+        File f = new File("D:/1.exe");
 
         try{
-            System.out.println("试图打开c:/lol.exe");
+            System.out.println("试图打开D:/lol.exe");
             new FileInputStream(f);
             System.out.println("成功打开");
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            Date d = sdf.parse("2016-06-03");
+            Date d = sdf.parse("2016-06:03");
         }catch (FileNotFoundException e){
             System.out.println("d:/1.exe不存在");
             e.printStackTrace();
