cf71b969df7ad70389e2e2ee3e6904bea766f5be
siweishengwu <ilu.zeff@gmail.com>
2020-04-23 23:46:55 +0800

    2020，4.23 change something

--- src/Mid_Level/I_O/File_Obeject/Step2.java
+++ src/Mid_Level/I_O/File_Obeject/Step2.java
@@ -1,6 +1,7 @@
 package Mid_Level.I_O.File_Obeject;
 
 import java.io.File;
+import java.util.Date;
 
 public class Step2 {
     public static void main(String[] args) {
@@ -19,5 +20,18 @@ public class Step2 {
 
         //文件长度
         System.out.println("获取文件的长度："+ f.length());
+
+        //文件最后修改时间
+        long time = f.lastModified();
+        Date d = new Date(time);
+        System.out.println("获取文件的最后修改时间："+d);
+        //设置文件修改时间为1970.1.1 08：00：00
+        f.setLastModified(0);
+
+        //文件重命名
+        File f2 = new File("d:/LOLFolder/DOTA.exe");
+        f.renameTo(f2);
+        System.out.println("把LOL.exe改成DOTA.exe");
+        System.out.println("注意：需要再D:\\LOLFolder确实存在一个LOL.exe，\r\n才可以看见对应的文件长度/修改时间等信息");
     }
 }
