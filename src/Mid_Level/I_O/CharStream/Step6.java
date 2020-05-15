d7c0b85697ada6e0b86ab1c9241dc363f63db102
siweishengwu <ilu.zeff@gmail.com>
2020-05-15 17:29:14 +0800

    2020，5.15 update some files

--- src/Mid_Level/I_O/CharStream/Step6.java
+++ src/Mid_Level/I_O/CharStream/Step6.java
@@ -1,7 +1,5 @@
 package Mid_Level.I_O.CharStream;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -15,59 +13,59 @@ public class Step6 {
     * @param decodedFile
     *       解密后保存的文件
     */
-//    public static void decodeFile(File decodingFile,File decodedFile) {
-//
-//        try(FileReader fr = new FileReader(decodingFile); FileWriter fw = new FileWriter(decodedFile)) {
-//            //读取源文件
-//            char[] fileContent = new char[(int) decodingFile.length()];
-//            fr.read(fileContent);
-//            System.out.println("源文件的内容：");
-//            System.out.println(new String(fileContent));
-//            //进行解密
-//            decode(fileContent);
-//            System.out.println("解密后的内容：");
-//            System.out.println(new String(fileContent));
-//            // 把解密后的内容保存到目标文件
-//            fw.write(fileContent);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    private static void decode(char[] fileContent) {
-//        for (int i = 0; i < fileContent.length; i++) {
-//            char c = fileContent[i];
-//            if (isLetterOrDigit(c)){
-//                switch (c) {
-//                    case '0':
-//                        c = '9';
-//                        break;
-//                    case 'a':
-//                        c = 'z';
-//                        break;
-//                    case 'A':
-//                        c = 'Z';
-//                        break;
-//                    default:
-//                        c--;
-//                        break;
-//                }
-//            }
-//            fileContent[i] = c;
-//        }
-//    }
-//
-//    public static boolean isLetterOrDigit(char c) {
-//        // 不使用Character类的isLetterOrDigit方法是因为，中文也会被判断为字母
-//        String letterOrDigital = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-//        return -1 == letterOrDigital.indexOf(c) ? false : true;
-//    }
-//
-//    public static void main(String[] args) {
-//        File decodingFile = new File("E:/project/j2se/src/Test2.txt");
-//        File decodedFile = new File("E:/project/j2se/src/Test1.txt");
-//
-//        decodeFile(decodingFile, decodedFile);
-//
-//    }
+    public static void decodeFile(File decodingFile,File decodedFile) {
+
+        try(FileReader fr = new FileReader(decodingFile); FileWriter fw = new FileWriter(decodedFile)) {
+            //读取源文件
+            char[] fileContent = new char[(int) decodingFile.length()];
+            fr.read(fileContent);
+            System.out.println("源文件的内容：");
+            System.out.println(new String(fileContent));
+            //进行解密
+            decode(fileContent);
+            System.out.println("解密后的内容：");
+            System.out.println(new String(fileContent));
+            // 把解密后的内容保存到目标文件
+            fw.write(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void decode(char[] fileContent) {
+        for (int i = 0; i < fileContent.length; i++) {
+            char c = fileContent[i];
+            if (isLetterOrDigit(c)){
+                switch (c) {
+                    case '0':
+                        c = '9';
+                        break;
+                    case 'a':
+                        c = 'z';
+                        break;
+                    case 'A':
+                        c = 'Z';
+                        break;
+                    default:
+                        c--;
+                        break;
+                }
+            }
+            fileContent[i] = c;
+        }
+    }
+
+    public static boolean isLetterOrDigit(char c) {
+        // 不使用Character类的isLetterOrDigit方法是因为，中文也会被判断为字母
+        String letterOrDigital = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        return -1 == letterOrDigital.indexOf(c) ? false : true;
+    }
+
+    public static void main(String[] args) {
+        File decodingFile = new File("E:/project/j2se/src/Test2.txt");
+        File decodedFile = new File("E:/project/j2se/src/Test1.txt");
+
+        decodeFile(decodingFile, decodedFile);
+
+    }
 }
