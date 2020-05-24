b04dd00589f1b8fa61d0ef8a4ca18fcf712bd718
siweishengwu <ilu.zeff@gmail.com>
2020-05-24 18:59:54 +0800

    2020，5.24 update some files

---
+++ src/Mid_Level/I_O/ObjectStream/Step1/Hero.java
@@ -0,0 +1,9 @@
+package Mid_Level.I_O.ObjectStream.Step1;
+
+import java.io.Serializable;
+
+public class Hero implements Serializable {
+    private static final long seriaVersionUID = 1l;
+    public String name;
+    public float hp;
+}
---
+++ src/Mid_Level/I_O/ObjectStream/Step1/TestStream.java
@@ -0,0 +1,36 @@
+package Mid_Level.I_O.ObjectStream.Step1;
+
+import java.io.*;
+import Mid_Level.I_O.ObjectStream.Step1.Hero;
+
+public class TestStream {
+
+    public static void main(String[] args) {
+        //创建一个Hero garen
+        //要把Hero对象直接保存在文件上，务必让Hero类实现Serializable接口
+        Hero h = new Hero();
+        h.name = "garen";
+        h.hp = 616;
+
+        //准备一个文件用于保存该对象
+        File f = new File("d:/garen.lol");
+
+        try(
+                //创建对象输出流
+                FileOutputStream fos = new FileOutputStream(f);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                //创建对象输入流
+                FileInputStream fis = new FileInputStream(f);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+           ){
+            oos.writeObject(h);
+            Hero h2 = (Hero) ois.readObject();
+            System.out.println(h2.name);
+            System.out.println(h2.hp);
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
