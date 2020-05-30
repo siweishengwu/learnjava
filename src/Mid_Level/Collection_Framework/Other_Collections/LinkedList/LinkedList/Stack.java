9b7bbd70b4dc8c9457912aa3c8f7d518181f120d
siweishengwu <ilu.zeff@gmail.com>
2020-05-30 18:35:30 +0800

    2020，5.30 update some files

---
+++ src/Mid_Level/Collection_Framework/Other_Collections/LinkedList/LinkedList/Stack.java
@@ -0,0 +1,13 @@
+package Mid_Level.Collection_Framework.Other_Collections.LinkedList.LinkedList;
+
+import Mid_Level.Collection_Framework.ArrayList.Generic.Step5.Hero;
+
+public interface Stack {
+
+    //把英雄推入到最后位置
+    public void push(Hero h);
+    //把最后一个英雄取出来
+    public Hero pull();
+    //查看最后一个英雄
+    public Hero peek();
+}
