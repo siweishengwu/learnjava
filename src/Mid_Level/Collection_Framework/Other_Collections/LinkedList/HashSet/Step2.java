6bcc36465d7892f25998efac0e6e3c36664e9b8e
siweishengwu <ilu.zeff@gmail.com>
2020-06-01 22:58:27 +0800

    2020，6.1 update some files

---
+++ src/Mid_Level/Collection_Framework/Other_Collections/LinkedList/HashSet/Step2.java
@@ -0,0 +1,17 @@
+package Mid_Level.Collection_Framework.Other_Collections.LinkedList.HashSet;
+
+import java.util.HashSet;
+
+public class Step2 {
+    public static void main(String[] args) {
+        HashSet<Integer> numbers = new HashSet<Integer>();
+
+        numbers.add(5);
+        numbers.add(9);
+        numbers.add(1);
+        numbers.add(2);
+
+        // Set中的元素排列，不是按照插入顺序
+        System.out.println(numbers);
+    }
+}
